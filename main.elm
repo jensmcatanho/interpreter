6c9404714e4eafb307d51d95fc2e66edc5b5135a
Jean Catanho <jensmcatanho@gmail.com>
2016-12-11 22:07:27 -0200

    Add the example from the slide

--- main.elm
+++ main.elm
@@ -61,6 +61,17 @@ evalProg prog env =
 lang : Prog -> Int
 lang p = ((evalProg p zero) "ret")
 
+-- p1 returns x if it's been declared and 100 otherwise. 
+p1 : Prog
+p1 = Seq
+        (Attr "x" (Add (Num 11) (Num 9)))
+--        (Attr "y" (Add (Num 11) (Num 9)))
+
+        (If_Else (Var "x")
+            (Attr "ret" (Var "x"))
+            (Attr "ret" (Num 100))
+        )
+
 -- p2 calculates the sum of the first n-1 natural numbers.
 p2 : Prog
 p2 = Seq
@@ -76,4 +87,4 @@ p2 = Seq
             )
         )
 
-main = text (toString (lang p2))
+main = text (toString (lang p1))
